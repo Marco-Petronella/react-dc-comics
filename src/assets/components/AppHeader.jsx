ecb3d76f90c5150b3c1144cd6697228443b94201
Marco-Petronella <marco.petronella85@gmail.com>
2026-04-20 14:01:08 +0200

    install bootstrap, nav placeholder

--- src/assets/components/AppHeader.jsx
+++ src/assets/components/AppHeader.jsx
@@ -1,5 +1,83 @@
+import logo from "../img/dc-logo.png";
+
 export default function AppHeader() {
     return (
-        <header>questo è l'header</header>
-    )
+        <header>
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <img src={logo} alt="" />
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Home
+                            </a>
+                        </div>
+                    </div>
+                    <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Comics
+                            </a>
+                        </div>
+                    </div>
+                    <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Movies
+                            </a>
+                        </div>
+                    </div>
+                    <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Tv
+                            </a>
+                        </div>
+                    </div>
+                                        <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Games
+                            </a>
+                        </div>
+                    </div>
+                                        <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Collectibles
+                            </a>
+                        </div>
+                    </div>
+                                        <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Videos
+                            </a>
+                        </div>
+                    </div>
+                                        <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Fans
+                            </a>
+                        </div>
+                    </div>
+                                        <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                News
+                            </a>
+                        </div>
+                    </div>
+                                        <div className="collapse navbar-collapse" id="navbarID">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">
+                                Shop
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 }
