cc72919983d8a1522a28b6753aa4b194854293a8
Marco-Petronella <marco.petronella85@gmail.com>
2026-04-20 14:17:45 +0200

    main logic for listing comics

--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,18 +1,18 @@
-import comics from '../script/comics.js'
+import comics from "../script/comics.js";
 
 export default function AppMain() {
     return (
         <main>
-            questo è il main
-            
+            <div className="container row justify-content-center align-items-center g-2 flex-wrap" >    
             {comics.map((albo) => (
-               <div key={albo.id}>
-                {albo.title}
-                <p>costa {albo.price}</p>
+                <div key={albo.id} className="card col-2">
+                    <img src={albo.thumb} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{albo.title} {albo.price}</h5>
+                    </div>
                 </div>
-                
-            ))}
-        
-            </main>
-    )
+            ))}                    
+        </div>
+        </main>
+    );
 }
