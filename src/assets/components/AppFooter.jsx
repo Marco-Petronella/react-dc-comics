dbb294fc80b11181e7324a4000642fecf3f19207
Marco-Petronella <marco.petronella85@gmail.com>
2026-04-20 13:32:11 +0200

    logic for elements on main

--- src/assets/components/AppFooter.jsx
+++ src/assets/components/AppFooter.jsx
@@ -1,5 +1,27 @@
+// const elementiDinamic = [
+//     {
+//         id: 1,
+//         title: "superman",
+//         path: "/#superman"
+//     },
+//     {
+//         id: 2,
+//         title: "batman",
+//         path: "/#batman"
+//     },
+//     {
+//         id: 3,
+//         title: "wolverine",
+//         path: "/#wolv"
+//     }
+// ]
+
 export default function AppFooter() {
     return (
-        <footer>questo è il footer</footer>
+        <footer>questo è il footer <br />
+        
+        {/* {elementiDinamic.map((item) => (<a key={item.id} href={item.path}>{item.title}</a>))} */}
+        </footer>
+        
     )
 }
--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,5 +1,18 @@
+import comics from '../script/comics.js'
+
 export default function AppMain() {
     return (
-        <main>questo è il main</main>
+        <main>
+            questo è il main
+            
+            {comics.map((albo) => (
+               <div key={albo.id}>
+                {albo.title}
+                <p>costa {albo.price}</p>
+                </div>
+                
+            ))}
+        
+            </main>
     )
 }
